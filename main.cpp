71a4e7da7db4800596898be17761d100dd98ad0b
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2022-10-08 21:53:11 -0700

    Added insert_board function, which allows the user to type in a input that will insert the starting numbers for sudoku.

--- main.cpp
+++ main.cpp
@@ -19,9 +19,57 @@ void print_Board(vector<vector<int>> board){
         cout<<endl<<"-----------------------------------"<<endl;
     }
 }
+void insert_board(vector<vector<int>>& board, const string& input){
+    int row, col, val, i = 0;
+    char pick;
+    while(i < input.size()){
+        if(input[i] == '('){
+            pick = 'r';
+            i++;
+            continue;
+        }
+        else if(input[i] == ','){
+            pick = 'c';
+            i++;
+            continue;
+        }
+        else if (input[i] == ':'){
+            pick = 'v';
+            i++;
+            continue;
+        }
+        else if(input[i] == ')') {
+            board[row][col] = val;
+            row, col, val = 0;
+            i++;
+            continue;
+        }
+        switch(pick){
+            case 'r':
+                row = input[i] - '0';
+                break;
+            case 'v':
+                val = input[i] - '0';
+                break;
+            case 'c':
+                col = input[i] - '0';
+                break;
+            default:
+                break;
+        }
+        i++;
+    }
+
+
+}
+
 int main() {
     vector<vector<int>> board(9,vector<int>(9));
     initialize_Board(board);
+    string input;
+    cout<<"Enter in the intitial board using this style (row,col:val):  ";
+    cin>> input;
+    insert_board(board,input);
 
     cout<<endl<<endl;
     //Printing Board
@@ -29,7 +77,13 @@ int main() {
     return 0;
 
 }
-/*
+/*String Input style:
+ * (row,col:val)(row,col:val)
+ * 0 1 2 3 4 5 6 7
+ *
+ *
+ *
+ * Board:
  * _______________________________________
  * | 0 | 0 | 0 || 0 | 0 | 0 || 0 | 0 | 0 |
  * | 0 | 0 | 0 || 0 | 0 | 0 || 0 | 0 | 0 |
